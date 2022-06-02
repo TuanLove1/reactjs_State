ec9bcef69463eb9674df0ba007a5bc5d926df3c0
TuanLove1 <shanksnguyenhuu@gmail.com>
2022-06-02 11:51:31 +0700

    upup

--- src/component/index.js
+++ src/component/index.js
@@ -7,102 +7,62 @@ export default class State extends Component {
     this.state = {
       data,
       detail: data[0],
-    };
+    }
   }
-  // change state of detail when click on button
   handleChange = (data) => {
     this.setState({
       detail: data,
-    });
-  };
+    })
+  }
+  // change state of detail when click on button
+
   render() {
     const { data, detail } = this.state;
     return (
       <div className="main">
-        <div className="container">
-          <h1 className="text-center">TRY CLASSES APP ONLINE</h1>
-          <div className="model">
-            <div className="banner">
-              <img src="./img/model.jpg" alt="" />
+        <div className="row mini">
+          <div className="col-md-4 content">
+            <div className="content__img">
+              <h1>TRY CLASSES APP ONLINE</h1>
+              <img className="img-fluid" src="./img/model.jpg" alt />
             </div>
-            <div className="title">
-              <img src={detail.url} className="img-fluid" alt="" />
-              <h2>{detail.name}</h2>
-              <p>{detail.desc}</p>
+            <div className="text">
+              <img className="img-fluid" src={detail.url} alt />
+              <p>{detail.name}</p>
+              <p>
+                {detail.desc}.
+              </p>
             </div>
           </div>
-          <div className="row">
-            <div className="col-2">
-              <img
-                src="./img/v1.png"
-                className="img-fluid"
-                alt=""
-                onClick={() => this.handleChange(data[0])}
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v2.png"
-                className="img-fluid"
-                alt=""
-                onClick={() => this.handleChange(data[1])}
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v8.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[2])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v6.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[3])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v7.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[4])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v6.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[5])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v7.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[6])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v8.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[7])}
-                alt=""
-              />
-            </div>
-            <div className="col-2">
-              <img
-                src="./img/v9.png"
-                className="img-fluid"
-                onClick={() => this.handleChange(data[8])}
-                alt=""
-              />
+          <div className="col-md-8 kinh">
+            <div className="row align-items-center">
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[0]) }} src="./img/v1.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[1]) }} src="./img/v2.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[2]) }} src="./img/v3.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[3]) }} src="./img/v4.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[4]) }} src="./img/v5.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[5]) }} src="./img/v6.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[6]) }} src="./img/v7.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[7]) }} src="./img/v8.png" alt />
+              </div>
+              <div className="col-2">
+                <img className="img-fluid" onClick={() => { this.handleChange(data[8]) }} src="./img/v9.png" alt />
+              </div>
             </div>
           </div>
         </div>
